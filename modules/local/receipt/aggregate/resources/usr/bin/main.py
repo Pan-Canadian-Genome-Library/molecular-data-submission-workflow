70a9c105de2f65e26d5b42ec8b8a9902c08f6e12
edsu7 <22638361+edsu7@users.noreply.github.com>
2025-10-10 16:37:36 -0400

    add feature analysis failure point in receipt
    addressing https://github.com/Pan-Canadian-Genome-Library/molecular-data-submission-workflow/issues/21

--- modules/local/receipt/aggregate/resources/usr/bin/main.py
+++ modules/local/receipt/aggregate/resources/usr/bin/main.py
@@ -46,6 +46,17 @@ def generate_batch_tsv(receipts, batch_id, output_file):
             study_id = receipt.get('study_id', None)
             analysis_state = receipt.get('analysis_state', None)
             published_at = receipt.get('published_at', None)
+            process_failure_point = None
+
+            if receipt.get("processes"):
+                failed_processes=[]
+                for process in receipt.get('processes'):
+                    if process.get('status'):
+                        if process.get('status')==False:
+                            failed_processes.append(process.get("process"))
+                
+                if not overall_status:
+                    process_failure_point = failed_processes[-1]
 
             for process in receipt.get('processes', []):
                 writer.writerow([
